21bb4dad7bb16fa63fc0f6fa7cc2413978c98fef
DylanPrimera <36011245+DylanPrimera@users.noreply.github.com>
2019-11-27 14:10:32 -0500

    posts model

---
+++ middleware/WebApplication1/Models/Posts.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebApplication1.Models
+{
+    public class Postation
+    {
+
+            public ObjectId _id { get; set; }
+            public string ID { get; set; }
+            public string date { get; set; }
+            public string efficiency { get; set; }
+            public string likes { get; set; }
+            public string username { get; set; }
+        }
+    
+}
