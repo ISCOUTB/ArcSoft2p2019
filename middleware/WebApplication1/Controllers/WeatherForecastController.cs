1c09477a6990304ed07dca4b623f69d3d4e5e01f
Salas-Carlos <45397749+Salas-Carlos@users.noreply.github.com>
2019-11-26 21:00:19 -0500

    Verificación de usuario en la DB.
    Si no existe en la DB llama la API, en este ejemplo llamamos la de twitter y se agrega a la DB.

--- middleware/WebApplication1/Controllers/WeatherForecastController.cs
+++ middleware/WebApplication1/Controllers/WeatherForecastController.cs
@@ -30,31 +30,28 @@ namespace WebApplication1.Controllers
         {
             string Usuario = HttpContext.Request.Query["Usuario"];
             string RedSocial = HttpContext.Request.Query["RedSocial"];
-
-            var Client = new WebClient().DownloadString("https://sunnypropercygwin--carlossalas2.repl.co/api/v1/users?");
             var Servidor = new MongoClient("mongodb://RedSocial:3015665692@redsocial-shard-00-00-ouis9.mongodb.net:27017,redsocial-shard-00-01-ouis9.mongodb.net:27017,redsocial-shard-00-02-ouis9.mongodb.net:27017/test?ssl=true&replicaSet=RedSocial-shard-0&authSource=admin&retryWrites=true&w=majority");
-            
-            var Database= Servidor.GetDatabase("RedSocial");
-
-            var collection = Database.GetCollection<Reservation>("Usuarios");
+            var Database = Servidor.GetDatabase("RedSocial");
+            var collection = Database.GetCollection<Reservation>(RedSocial);
        
-            var list = JsonConvert.DeserializeObject(Client);
 
 
-
-            IList<Reservation> documents = BsonSerializer.Deserialize<IList<Reservation>>(Client);
-            collection.InsertOne(documents[0]);
+            //IList<Reservation> documents = BsonSerializer.Deserialize<IList<Reservation>>(Client);
+           
 
             var query = collection.AsQueryable<Reservation>();
             var result = from n in query
                          where n.Username == "t00045753"
                          select n;
 
-            if (result != null)
-                return result;
-            else
-                return "[]";
+           if(!result.Any()){
+                var Client = new WebClient().DownloadString("https://aonobird.aztrarok.repl.co/twitter/user/" + Usuario);
+                var documents = BsonSerializer.Deserialize<Reservation>(Client);
+                collection.InsertOne(documents);
 
+                return documents;
+            }
+           return Ok(result);
 
 
         }
