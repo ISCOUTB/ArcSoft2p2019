65ca322c5c6c10fe4541fdca88605baecf55792a
Salas-Carlos <45397749+Salas-Carlos@users.noreply.github.com>
2019-11-27 09:33:07 -0500

    Estructura para users

--- middleware/WebApplication1/Models/Api.cs
+++ middleware/WebApplication1/Models/Api.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Bson;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +9,10 @@ namespace WebApplication1.Models
     public class Reservation
     {
         public ObjectId _id { get; set; }
-        public string Email { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Password { get; set; }
-        public string Username { get; set; }
+        public string followers { get; set; }
+        public string fullname { get; set; }
+        public string ID { get; set; }
+        public string post { get; set; }
+        public string username { get; set; }
     }
 }
