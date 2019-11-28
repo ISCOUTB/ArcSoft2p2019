a57b6ccbc0d966281074261c9ef78460dfea490d
DylanPrimera <36011245+DylanPrimera@users.noreply.github.com>
2019-11-27 22:21:14 -0500

    Update WeatherForecastController.cs

--- middleware/WebApplication1/Controllers/WeatherForecastController.cs
+++ middleware/WebApplication1/Controllers/WeatherForecastController.cs
@@ -33,7 +33,7 @@ namespace WebApplication1.Controllers
             var Servidor = new MongoClient("mongodb://RedSocial:3015665692@redsocial-shard-00-00-ouis9.mongodb.net:27017,redsocial-shard-00-01-ouis9.mongodb.net:27017,redsocial-shard-00-02-ouis9.mongodb.net:27017/test?ssl=true&replicaSet=RedSocial-shard-0&authSource=admin&retryWrites=true&w=majority");
             var Database = Servidor.GetDatabase("RedSocial");
             var collection = Database.GetCollection<Reservation>("Twitter");
-            //var collectionPosts = Database.GetCollection<Postation>("Twitter");
+            var collectionPosts = Database.GetCollection<Postation>("Twitter");
        
 
 
@@ -42,29 +42,30 @@ namespace WebApplication1.Controllers
 
             var query = collection.AsQueryable<Reservation>();
             var result = from n in query
-                         where n.username == "Rubiu5"
+                         where n.username == Usuario
                          select n;
-            /*
+                         
             var queryPosts = collectionPosts.AsQueryable<Postation>();
             var resultPost = from t in queryPosts
-                             where t.user == "Rubiu5"
+                             where t.username == Usuario
                              select t;
-            
-            */
 
            if(!result.Any()){
-               var Client = new WebClient().DownloadString("https://aonobird.aztrarok.repl.co/user/Rubiu5");
+                var Client = new WebClient().DownloadString("https://aonobird.aztrarok.repl.co/user/"+Usuario);
                 var documents = BsonSerializer.Deserialize<Reservation>(Client);
                 var Posts = new WebClient().DownloadString("https://aonobird.aztrarok.repl.co/posts/");
                 IList<Postation> posts = BsonSerializer.Deserialize<IList<Postation>>(Posts);
                 List<object> lista = new List<object>();
                 lista.Add(documents);
                 lista.Add(posts);
-                //collection.InsertOne(documents);
-                //collectionPosts.InsertMany(posts);
+                collection.InsertOne(documents);
+                collectionPosts.InsertMany(posts);
                 return lista;
+            }else{
+                return Ok(result);
             }
-           return Ok(result);
+            
+           
 
 
         }
