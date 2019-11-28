957bd41f54c29f6c01bf54240844816a687568a8
Heiner Acosta <hacm1997@gmail.com>
2019-11-28 12:51:32 -0500

    respuesta Json

--- reddit/app/Http/Controllers/UserController.php
+++ reddit/app/Http/Controllers/UserController.php
@@ -79,6 +79,7 @@ class UserController extends Controller
         $me = $name;
         //$me2 = 'radijak';
         //$me3 = 'Bitcoin';
+        // BalthazarBulldozer - nothing_to_feel_here
 
         $url ='https://ssl.reddit.com/api/v1/access_token';
         $clientId = 'OrDWl9bHpw0_eA';
@@ -152,7 +153,7 @@ class UserController extends Controller
         curl_close($curl);
 
         $response = json_decode($response, true);
-        //var_dump($response);
+        var_dump($response);
         //The last 5 posts
         $p1 = $response['data']['children']['0']['data']['link_title'];
         $p2 = $response['data']['children']['1']['data']['link_title'];
@@ -182,5 +183,98 @@ class UserController extends Controller
                             'up1' => $up1, 'up2' => $up2, 'up3' => $up3, 'up4' => $up4, 'up5' => $up5,
                             'sumpost' => $sumpost]);
     
+    }
+
+    
+
+    public function dataJson($name)
+    {
+        
+        $me = $name;
+
+        $url ='https://ssl.reddit.com/api/v1/access_token';
+        $clientId = 'OrDWl9bHpw0_eA';
+        $clientSecret = 'GUcXmPqvvQ5nZuXZnjzNNufvT8o';
+
+        // post variables
+        $fields = array (
+            'grant_type' => 'client_credentials'
+        );
+
+        $userAgent = 'sometext:appnamehere v0.1 by usernamehere';
+
+        // prepare data for post
+        $field_string = http_build_query($fields);
+
+        $curl2 = curl_init($url);
+        curl_setopt($curl2, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . base64_encode($clientId . ':' . $clientSecret) ));
+        curl_setopt($curl2,CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl2,CURLOPT_USERAGENT, $userAgent);
+        curl_setopt($curl2,CURLOPT_POST, 1);
+        curl_setopt($curl2,CURLOPT_POSTFIELDS, $field_string);
+
+        $response2 = curl_exec($curl2);
+        $err2 = curl_error($curl2);
+        curl_close($curl2);
+
+        $response2 = json_decode($response2, true);
+        //var_dump($response); // access_token should be here
+
+        // now get the data
+        $curl2 = curl_init('https://oauth.reddit.com/user/'.$me.'/about');
+        curl_setopt( $curl2, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $response2['access_token'] ) );
+        curl_setopt($curl2,CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl2,CURLOPT_USERAGENT, $userAgent);
+
+        $response2 = curl_exec($curl2);
+        $err2 = curl_error($curl2);
+        curl_close($curl2);
+
+        $response2 = json_decode($response2, true);
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . base64_encode($clientId . ':' . $clientSecret) ));
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl,CURLOPT_USERAGENT, $userAgent);
+        curl_setopt($curl,CURLOPT_POST, 1);
+        curl_setopt($curl,CURLOPT_POSTFIELDS, $field_string);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        $response = json_decode($response, true);
+        //var_dump($response); // access_token should be here
+
+        // now get the data & Authentication
+        $curl = curl_init('https://oauth.reddit.com/user/'.$me.'/overview');
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $response['access_token'] ) );
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl,CURLOPT_USERAGENT, $userAgent);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        $response = json_decode($response, true);
+        //var_dump($response);
+
+        $p1 = $response['data']['dist'];
+
+        $post1 = strval($p1);
+
+        //$allpost[] = array('p1' => $p1, 'p2' => $p2, 'p3' => $p3, 'p4' => $p4, 'p5' => $p5);
+
+        $ident = $response2['data']['id'];
+
+        $fullname = $response['data']['children']['0']['data']['author_fullname'];
+
+        $author = $response['data']['children']['0']['data']['author'];
+
+        $datos[] = array('followers'=> '0', 'fullname' => $fullname, 'ID' => $ident, 'post' => $post1, 'username' => $author);
+
+        $json_string = json_encode($datos);
+        echo $json_string;
+
     }
 }
