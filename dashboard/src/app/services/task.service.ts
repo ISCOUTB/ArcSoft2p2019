1bd738824f37a33047ad4544a27de185ef314ca6
Jesus132 <36166541+Jesus132@users.noreply.github.com>
2019-12-01 21:42:10 -0500

    Quitando
    Quitando URL y funciones de pruebas que se tenia

--- dashboard/src/app/services/task.service.ts
+++ dashboard/src/app/services/task.service.ts
@@ -10,38 +10,17 @@ import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 export class TaskService {
 
  API='http://arcsoft2p2019.jairoe.co:8080/api/Datos?Usuario=';
-//  API=`http://arcsoft2p2019.jairoe.co:8080/api/Datos?Usuario=${u}&RedSocial=${r}`;
- API2='https://API.jesus132.repl'
+//API=`http://arcsoft2p2019.jairoe.co:8080/api/Datos?Usuario=${u}&RedSocial=${r}`;
 
   constructor(
     private http: HttpClient
   ) {}
 
   getAllTasks(u, r) {
-    console.log(u)
-    console.log(r)
     let A = this.API + u + '&RedSocial=' + r
-    let I2 = this.API2 + '.co'
     console.log(`-------u------${u}-------------------------`)
-    console.log(A)
-    console.log(I2)
-    const path = I2;
+    const path = A;
     return this.http.get<Task>(path);
 }
 
-getTask(id: string) {
-  const path = `${this.API}/${id}`;
-  return this.http.get<Task>(path);
-}
-
-  async getAllTasks1() {
-    try {
-      let f=await fetch('https://SQLite.jesus132.repl.co');
-      let rF=await f.json();
-      console.log(rF);
-    } catch (error) {
-      console.log("Error: "+error); 
-    }
-    
-  }
 }
